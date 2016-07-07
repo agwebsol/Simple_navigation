a8ad8f10fc891cea6bec576e7a817743fce90353
daroomwanor <osirisdaro@gmail.com>
2016-07-07 14:16:10 -0400

    Update dashboard.php

--- Paycheckservice/application/views/template/dashboard.php
+++ Paycheckservice/application/views/template/dashboard.php
@@ -42,7 +42,7 @@
                                 $("#btnn").click(function(){
                                     $.ajax({
                                             type: 'POST',
-                                            url: 'http://52.40.215.168/Simple-Paycheck-Service/Paycheckservice/',
+                                            url: 'http://52.40.215.168/Simple-Paycheck-Service/Paycheckservice/ajax_estimate_app',
                                             success: function(data){
                                                     $("#data").html(data);
                                             }
