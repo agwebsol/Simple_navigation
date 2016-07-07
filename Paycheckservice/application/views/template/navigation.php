a61ca8cdbf1abad5ff78f5ec689187344acb06b1
daroomwanor <osirisdaro@gmail.com>
2016-07-07 18:22:12 -0400

    Update navigation.php

--- Paycheckservice/application/views/template/navigation.php
+++ Paycheckservice/application/views/template/navigation.php
@@ -1,5 +1,5 @@
 <!--Navigation-->
-<a class="w3-padding-16" href="?index.php/App_Controller/index"><i class="fa fa-home w3-xxlarge"></i></a>
-<a class="w3-padding-16" href="?index.php/App_Controller/add_employee"><i class="fa fa-user-plus w3-xxlarge"></i></a>
-<a class="w3-padding-16" href="?index.php/App_Controller/view_all_employee"><i class="fa fa-group w3-xxlarge"></i></a>
-<a class="w3-padding-16" href="?index.php/App_Controller/submit_hours"><i class="fa fa-calendar-plus-o w3-xxlarge"></i></a>
+<a class="w3-padding-16" href="index.php/App_Controller/index"><i class="fa fa-home w3-xxlarge"></i></a>
+<a class="w3-padding-16" href="App_Controller/add_employee"><i class="fa fa-user-plus w3-xxlarge"></i></a>
+<a class="w3-padding-16" href="/App_Controller/view_all_employee"><i class="fa fa-group w3-xxlarge"></i></a>
+<a class="w3-padding-16" href="index.php?App_Controller/submit_hours"><i class="fa fa-calendar-plus-o w3-xxlarge"></i></a>
