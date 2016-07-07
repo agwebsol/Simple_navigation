1475bd873220a46316755d0cd25e00dd464e1b6b
daroomwanor <osirisdaro@gmail.com>
2016-07-06 22:59:17 -0400

    Update view_employee.php

--- Paycheckservice/application/views/template/view_employee.php
+++ Paycheckservice/application/views/template/view_employee.php
@@ -6,7 +6,7 @@
         </head>
         <body>
             <div>
-                <form method="POST" action="<? echo base_url(); ?>App_Controller/ajax_employee">
+                <form method="POST" action="ajax_employee">
                      <table class="table table-hover">
                          <?php
                             foreach($employee as $data)
@@ -15,49 +15,49 @@
                                 ?>
                                     <tr>
                                         <th>Firstname</th>
-                                        <td><input type="text" name="fname" value ="<? echo $data["Firstname"]; ?>"</td>
+                                        <td><input type="text" name="fname" value ="<?php echo $data["Firstname"]; ?>"</td>
                                     </tr>
                                     <tr>
                                         <th>Lastname</th>
-                                        <td><input type="text" name="lname" value ="<? echo $data["Lastname"]; ?>"></td>
+                                        <td><input type="text" name="lname" value ="<?php echo $data["Lastname"]; ?>"></td>
                                     </tr>
                                     <tr>
                                         <th>Other Names</th>
-                                        <td><input type="text" name="oname" value ="<? echo $j_data["other_name"]; ?>"></td>
+                                        <td><input type="text" name="oname" value ="<?php echo $j_data["other_name"]; ?>"></td>
                                     </tr>
                                     <tr>
                                         <th>Address 1</th>
-                                        <td><input type="text" name="address1" value ="<? echo $j_data["address1"]; ?>"></td>
+                                        <td><input type="text" name="address1" value ="<?php echo $j_data["address1"]; ?>"></td>
                                     </tr>
                                     <tr>
                                         <th>Address 2</th>
-                                        <td><input type="text" name="address2" value ="<? echo $j_data["address2"]; ?>"></td>
+                                        <td><input type="text" name="address2" value ="<?php echo $j_data["address2"]; ?>"></td>
                                     </tr>
                                     <tr>
                                         <th>City</th>
-                                        <td><input type="text" name="city" value ="<? echo $j_data["city"]; ?>"></td>
+                                        <td><input type="text" name="city" value ="<?php echo $j_data["city"]; ?>"></td>
                                     </tr>
                                     <tr>
                                         <th>State</th>
-                                        <td><input type="text" name="state" value ="<? echo $j_data["state"]; ?>"></td>
+                                        <td><input type="text" name="state" value ="<?php echo $j_data["state"]; ?>"></td>
                                     </tr>
                                     <tr>
                                         <th>Zip Code</th>
-                                        <td><input type="text" name="zip" value ="<? echo $j_data["zip_code"]; ?>"></td>
+                                        <td><input type="text" name="zip" value ="<?php echo $j_data["zip_code"]; ?>"></td>
                                     </tr>
                                     <tr>
                                         <th>Social Security</th>
-                                        <td><input type="text" name="sos" value ="<? echo $data["SSN"]; ?>"></td>
+                                        <td><input type="text" name="sos" value ="<?php echo $data["SSN"]; ?>"></td>
                                     </tr>
                                     <tr>
                                         <th>Date Of Birth</th>
-                                        <td><input type="date" name="dob" value ="<? echo $data["DOB"]; ?>"></td>
+                                        <td><input type="date" name="dob" value ="<?php echo $data["DOB"]; ?>"></td>
                                     </tr>
                                     <tr>
                                         <th>Marital Status</th>
                                         <td>
                                             <select name="status">
-                                                <option value="<? echo $data['Status']; ?>">--Select--</option>
+                                                <option value="<?php echo $data['Status']; ?>">--Select--</option>
                                                 <option value="Single">Single</option>
                                                 <option value="Married">Married</option>
                                                 <option value="Divorced">Divorced</option>
@@ -67,18 +67,18 @@
                                     </tr>
                                     <tr>
                                         <th>Employee Rate</th>
-                                        <td><input type="text" name="hour_rate" value ="<? echo $data["Hour_rate"]; ?>"></td>
-                                        <td><input type="text" name="salary" value ="<? echo $data["Salary"]; ?>"></td>
+                                        <td><input type="text" name="hour_rate" value ="<?php echo $data["Hour_rate"]; ?>"></td>
+                                        <td><input type="text" name="salary" value ="<?php echo $data["Salary"]; ?>"></td>
                                     </tr>
                                     <tr>
                                         <th>Employee Start Date</th>
-                                        <td><input type="date" name="start_date" value ="<? echo $data["Start_date"]; ?>"></td>
+                                        <td><input type="date" name="start_date" value ="<?php echo $data["Start_date"]; ?>"></td>
                                     </tr>
                                     <tr>
-                                        <th><input type="hidden" name="id" value="<? echo $data["ID"]; ?>"></th>
+                                        <th><input type="hidden" name="id" value="<?php echo $data["ID"]; ?>"></th>
                                         <td><input type="submit" name="submit" value="Update Employee" ></td>
                                     </tr>
-                                <?
+                                <?php
                             }
                          ?>
                      </table>
