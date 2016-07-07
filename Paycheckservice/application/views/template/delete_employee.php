c8021cb402151381dbe08d9e93a11c889a45035e
daroomwanor <osirisdaro@gmail.com>
2016-07-06 22:52:25 -0400

    Update delete_employee.php

--- Paycheckservice/application/views/template/delete_employee.php
+++ Paycheckservice/application/views/template/delete_employee.php
@@ -1,20 +1,20 @@
 <!---Prompt to Delete Employee--->
 <body>
     
-    <form method="POST" action="<? base_url(); ?>ajax_delete_employee">
+    <form method="POST" action="ajax_delete_employee">
         <table>
             <?php
                 foreach($employee as $data)
                 {
                     ?>
                         <tr>
-                            <th>Add You Sure You Want To Delete <? echo $data['Firstname'].' '. $data['Lastname']; ?> ? </th>
+                            <th>Add You Sure You Want To Delete <?php echo $data['Firstname'].' '. $data['Lastname']; ?> ? </th>
                         </tr>
                         <tr>
-                            <th><input type="hidden" name="id" value="<? echo $data['ID']; ?>"></th>
+                            <th><input type="hidden" name="id" value="<?php echo $data['ID']; ?>"></th>
                             <td><input class="btn btn-info" type="submit" name="delete" value="Confirm Delete"></td>
                         </tr>
-                    <?
+                    <?php
                 }
             ?>
         </table>
